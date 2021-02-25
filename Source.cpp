d5c8a0e9c5a16f941d2b505804290a5f3d783b15
HiLMadara <71180913+HiLMadara@users.noreply.github.com>
2021-02-25 22:23:17 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+#include <vector>;
+using namespace std;
+class fVector
+{
+    std::vector<float> v;
+public:
+    fVector(int size)
+    {
+        v = vector<float>(size);
+    }
+};
+
+main(
+
+
+)
